523fc296646b9313251f435147f132216c94336a
Diman0 <52963853+Diman0@users.noreply.github.com>
2020-02-11 23:06:56 +0100

    Create restoreBackupContainer.sh
    Added example script for restoring backup of citadel data volume.

---
+++ examples/restoreBackupContainer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "MOUNTING /backup as /backup."
+echo "Place backups in this folder"
+echo "Stopping citadel."
+docker-compose -p mail -f /scripts/docker/docker-compose.yml up -d --no-recreate --scale citadel=0
+echo "Starting backup container"
+echo "Example extract command: tar -x -f /backup/backupCitadel.tar.gz -v "
+docker run -it --rm -v /backup/:/backup -v citadel-data:/usr/local/citadel/data -v citadel-alias:/usr/local/citadel/network debian:stretch /bin/bash
+echo "Starting citadel"
+docker-compose -p mail -f /scripts/docker/docker-compose.yml up -d --no-recreate --scale citadel=1
+echo  "Done "; date
