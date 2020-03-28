768cb961934807f2162287a1451e0de9de3287f6
Diman0 <52963853+Diman0@users.noreply.github.com>
2020-03-28 16:16:01 +0100

    Update restoreBackupContainer.sh
    removed reference to mail.alias since this is not used anymore by citadel

--- examples/restoreBackupContainer.sh
+++ examples/restoreBackupContainer.sh
@@ -5,7 +5,7 @@ echo "Stopping citadel."
 docker-compose -p mail -f /scripts/docker/docker-compose.yml up -d --no-recreate --scale citadel=0
 echo "Starting backup container"
 echo "Example extract command: tar -x -f /backup/backupCitadel.tar.gz -v "
-docker run -it --rm -v /backup/:/backup -v citadel-data:/usr/local/citadel/data -v citadel-alias:/usr/local/citadel/network debian:stretch /bin/bash
+docker run -it --rm -v /backup/:/backup -v citadel-data:/usr/local/citadel/data debian:stretch /bin/bash
 echo "Starting citadel"
 docker-compose -p mail -f /scripts/docker/docker-compose.yml up -d --no-recreate --scale citadel=1
 echo  "Done "; date
